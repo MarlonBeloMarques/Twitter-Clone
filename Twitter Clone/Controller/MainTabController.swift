f9981ab958326a9b5c2220cfa6cabd23de0aae8e
Marlon Marques <marlon.belohd@gmail.com>
2022-05-24 21:07:40 -0300

    Configure Project with MainTabController in SceneDelegate

--- Twitter Clone/Controller/MainTabController.swift
+++ Twitter Clone/Controller/MainTabController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabController.swift
 //  Twitter Clone
 //
 //  Created by Marlon Marques on 24/05/22.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemPink
     }
 
-
 }
-
