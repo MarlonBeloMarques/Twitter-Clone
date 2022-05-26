13d9c0d6df2a1347e560452bfb9d64df20bd4ec2
Marlon Marques <marlon.belohd@gmail.com>
2022-05-25 21:58:45 -0300

    Create helper function to create navigationController

--- Twitter Clone/Controller/MainTabController.swift
+++ Twitter Clone/Controller/MainTabController.swift
@@ -24,19 +24,34 @@ class MainTabController: UITabBarController {
     func configureViewControllers() {
         
         let feed = FeedController()
-        feed.tabBarItem.image = UIImage(named: "home_unselected")
+        let nav1 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: feed)
         
         let explore = ExploreController()
-        explore.tabBarItem.image = UIImage(named: "home_unselected")
+        let nav2 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: explore)
 
         let notifications = NotificationsController()
-        notifications.tabBarItem.image = UIImage(named: "home_unselected")
+        let nav3 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: notifications)
 
         let conversations = ConversationsController()
-        conversations.tabBarItem.image = UIImage(named: "home_unselected")
-                
+        let nav4 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: conversations)
+
         configureTabBarBackgroundColor()
-        viewControllers = [feed, explore, notifications, conversations]
+        viewControllers = [nav1, nav2, nav3, nav4]
+    }
+    
+    private func templateNavigationController(image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = image
+        
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .white
+            nav.navigationBar.standardAppearance = appearance
+            nav.navigationBar.scrollEdgeAppearance = appearance
+        }
+                
+        return nav
     }
 
     private func configureTabBarBackgroundColor() {
