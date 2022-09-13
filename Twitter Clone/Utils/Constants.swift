06e7b2ffd1d79a3c8f82106e78c791f70c0b01fe
Marlon Marques <marlon.belohd@gmail.com>
2022-09-12 21:15:14 -0300

    Upload User Profile Image

---
+++ Twitter Clone/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Twitter Clone
+//
+//  Created by Marlon Marques on 12/09/22.
+//
+
+import Firebase
+
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGE = STORAGE_REF.child("profile_image")
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
