c7ad3eb7fdf5eeb3d30aef00b43590f306cb9c41
Marlon Marques <marlon.belohd@gmail.com>
2022-11-14 11:38:21 -0300

    Profile Header UI

---
+++ Twitter Clone/View/ProfileHeader.swift
@@ -0,0 +1,122 @@
+//
+//  ProfileHeader.swift
+//  Twitter Clone
+//
+//  Created by Marlon Marques on 09/11/22.
+//
+
+import UIKit
+
+class ProfileHeader: UICollectionReusableView {
+    
+    // MARK: - Properties
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "baseline_arrow_back_white_24dp")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleDismissAll), for: .touchUpInside)
+        return button
+    }()
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .lightGray
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.borderWidth = 4
+        
+        return imageView
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Loading", for: .normal)
+        button.layer.borderColor = UIColor.twitterBlue.cgColor
+        button.layer.borderWidth = 1.25
+        button.setTitleColor(UIColor.twitterBlue, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handleEditProfileFollow), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .twitterBlue
+        
+        view.addSubview(backButton)
+        backButton.anchor(top: view.topAnchor, left: view.leftAnchor, paddingTop: 42, paddingLeft: 16)
+        backButton.setDimensions(width: 30, height: 30)
+    
+        return view
+    }()
+    
+    private let bioLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 3
+        label.text = "This is a user bio that will span more than one line for test purposes"
+        
+        return label
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.text = "Marlon Marques"
+        return label
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .lightGray
+        label.text = "@bigbosss"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(containerView)
+        containerView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, height: 108)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: containerView.bottomAnchor, left: leftAnchor, paddingTop: -24, paddingLeft: 8)
+        profileImageView.setDimensions(width: 80, height: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: containerView.bottomAnchor, right: rightAnchor, paddingTop: 12, paddingRight: 12)
+        editProfileFollowButton.setDimensions(width: 100, height: 36)
+        editProfileFollowButton.layer.cornerRadius = 36 / 2
+        
+        let userDetailsStack = UIStackView(arrangedSubviews: [fullnameLabel, usernameLabel, bioLabel])
+        
+        userDetailsStack.axis = .vertical
+        userDetailsStack.distribution = .fillProportionally
+        userDetailsStack.spacing = 4
+        
+        addSubview(userDetailsStack)
+        userDetailsStack.anchor(top: profileImageView.bottomAnchor,
+                                left: leftAnchor, right: rightAnchor,
+                                paddingTop: 8, paddingLeft: 12, paddingRight: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleDismissAll() {
+        
+    }
+    
+    @objc func handleEditProfileFollow() {
+        
+    }
+}
